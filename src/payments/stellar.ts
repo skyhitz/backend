5fc83db15aa7a23810cc604f504e6407242ffce1
Brendan Sheehan <brendan@adp.bz>
2020-09-02 22:35:48 -0500

    fix subscription tokens

--- src/payments/stellar.ts
+++ src/payments/stellar.ts
@@ -126,6 +126,7 @@ export async function sendSubscriptionTokens(
   amount: number
 ) {
   const sourceAccount = await stellarServer.loadAccount(sourceKeys.publicKey());
+  const amountInString = `${amount}`;
   const transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
     fee: BASE_FEE,
     networkPassphrase: NETWORK_PASSPHRASE,
@@ -134,7 +135,7 @@ export async function sendSubscriptionTokens(
       StellarSdk.Operation.payment({
         destination: destinationKey,
         asset: asset,
-        amount: amount.toString(),
+        amount: amountInString,
       })
     )
     .setTimeout(0)
