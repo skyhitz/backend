5d7f359c7dd8118389ed7abf2a3a16046aaf3065
Brendan Sheehan <brendan@adp.bz>
2020-09-03 18:04:12 -0500

    sleep 500 ms

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -62,13 +62,13 @@ async function onCustomerCreated({ object }: any, response) {
   }
 }
 
-// async function sleep(ms) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 async function onChargeSucceeded({ object }: any, response) {
   const { receipt_email, amount } = object;
-  // await sleep(100);
+  await sleep(500);
   const { metadata } = await findCustomer(receipt_email);
   const { publicAddress, seed } = metadata;
 
