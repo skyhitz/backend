98fd43a87c199548c91994682c82b4b3eaf1e96e
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-01-19 09:44:13 -0500

    fix routes on vercel

--- src/assets.ts
+++ src/assets.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import { getAll } from './redis';
-// const TOML = require('@iarna/toml');
-// var tom = require('toml-js');
 
 export function assets(graphQLServer) {
   graphQLServer.get(
-    '/api/assets/:cid',
+    '/api/assets/:cid/.well-known/stellar.toml',
     express.raw({ type: 'application/toml' }),
     async (request: express.Request, response: express.Response) => {
       const cid = request.params.cid;
       const toml = await getAll(`toml:${cid}`);
       if (toml) {
-        // console.log(JSON.parse(toml.toml));
         return response.send(toml.toml);
       }
       return response.send(404);
