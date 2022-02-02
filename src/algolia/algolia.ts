0deb33546498bf5ffb4c094b1b8675ebaec9ecf6
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-02-02 13:52:11 -0500

    update algolia indexes

--- src/algolia/algolia.ts
+++ src/algolia/algolia.ts
@@ -4,8 +4,8 @@ const client = algoliasearch(
   Config.ALGOLIA_APP_ID,
   Config.ALGOLIA_ADMIN_API_KEY
 );
-export const entriesIndex = client.initIndex('entries');
-export const usersIndex = client.initIndex('users');
+export const entriesIndex = client.initIndex(`${Config.APP_URL}:entries`);
+export const usersIndex = client.initIndex(`${Config.APP_URL}:users`);
 
 // Always pass objectID
 export async function partialUpdateObject(obj: any) {
