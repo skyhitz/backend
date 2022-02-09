00624470193f28f98e08f3baa231dfaa367ed34a
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-02-09 10:06:33 -0500

    assets meta server

--- src/graphql/recently-added.ts
+++ src/graphql/recently-added.ts
@@ -1,63 +1,14 @@
 import { GraphQLList } from 'graphql';
 import Entry from './types/entry';
 import { getAuthenticatedUser } from '../auth/logic';
-import { sendCommand } from '../redis';
-import { chunk } from '../util/chunk';
+import { recentlyAdded } from '../redis';
 
 const RecentlyAdded = {
   type: new GraphQLList(Entry),
   async resolve(root: any, args: any, ctx: any) {
     await getAuthenticatedUser(ctx);
-    let key = 'all-entries';
-    let entries = await sendCommand('sort', [
-      key,
-      'limit',
-      '0',
-      '20',
-      'by',
-      'entries:*->publishedAtTimestamp',
-      'desc',
-      'get',
-      'entries:*->imageUrl',
-      'get',
-      'entries:*->videoUrl',
-      'get',
-      'entries:*->description',
-      'get',
-      'entries:*->title',
-      'get',
-      'entries:*->id',
-      'get',
-      'entries:*->forSale',
-      'get',
-      'entries:*->price',
-      'get',
-      'entries:*->artist',
-    ]);
 
-    return chunk(entries, 8).map(
-      ([
-        imageUrl,
-        videoUrl,
-        description,
-        title,
-        id,
-        forSale,
-        price,
-        artist,
-      ]) => {
-        return {
-          imageUrl: imageUrl,
-          videoUrl: videoUrl,
-          description: description,
-          title: title,
-          id: id,
-          forSale: forSale === 'true',
-          price: parseInt(price),
-          artist: artist,
-        };
-      }
-    );
+    return await recentlyAdded();
   },
 };
 
