505a035bcce4fd11de760dc337ce22afcfa33d57
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-04-12 14:06:10 -0700

    remove wait

--- src/algolia/algolia.ts
+++ src/algolia/algolia.ts
@@ -121,7 +121,7 @@ export async function getUserByPublicKey(publicKey: string) {
 }
 
 export async function saveUser(user) {
-  await usersIndex.saveObject(user).wait();
+  await usersIndex.saveObject(user);
 }
 
 export async function likeMulti(userId, entryId) {
