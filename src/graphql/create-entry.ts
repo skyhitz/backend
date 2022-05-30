153108fd919c32f16c78363b6948df747f04c0f5
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-05-30 13:02:31 -0500

    add user log

--- src/graphql/create-entry.ts
+++ src/graphql/create-entry.ts
@@ -44,6 +44,7 @@ const createEntry = {
   ) {
     let user = await getAuthenticatedUser(ctx);
     const addSellOffer = user.publicKey && forSale;
+    console.log('create entry: ', user);
 
     const keypairSeed = shajs('sha256')
       .update(Config.ISSUER_SEED + fileCid)
