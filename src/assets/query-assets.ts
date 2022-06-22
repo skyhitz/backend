5265501c45e17466b25423a2bb2dd326508adaba
Alejo Mendoza <alejomendoza@users.noreply.github.com>
2022-06-21 22:33:26 -0500

    Staging (#43)
    * Sync (#40)

---
+++ src/assets/query-assets.ts
@@ -0,0 +1,13 @@
+import { preparePagedData } from './list-helper';
+import { queryExternal } from './query-external';
+
+/**
+ * Query assets metadata.
+ * @param {AssetQueryModel} queryParams - Query parameters.
+ * @return {Promise<Array>}
+ */
+export function queryAssets(queryParams) {
+  return queryExternal(queryParams).then((res) =>
+    preparePagedData(queryParams, res)
+  );
+}
---
+++ src/assets/query-external.ts
@@ -0,0 +1,28 @@
+import { assetsMeta, findAssetMeta } from '../algolia/algolia';
+
+/**
+ * Query assets metadata from external resource.
+ * @param {AssetQueryModel} queryParams - Query parameters.
+ * @return {Promise<Array>}
+ */
+export async function queryExternal(queryParams) {
+  // here the request to the external resource (database, private API etc) can be implemented
+  //  cursor = `${code}-${issuer}`
+  const { code, issuer, cursor, limit, order } = queryParams;
+
+  if (code || issuer) {
+    return await (
+      await findAssetMeta(code, issuer)
+    ).map(({ publishedAtTimestamp, ...rest }) => rest);
+  }
+
+  if (cursor) {
+    const [{ publishedAtTimestamp }] = await findAssetMeta(
+      cursor.substr(0, cursor.indexOf('-')),
+      cursor.substr(cursor.indexOf('-') + 1, cursor.length)
+    );
+    return (await assetsMeta(publishedAtTimestamp, limit + 1, order)).shift();
+  }
+
+  return await assetsMeta(0, limit, order);
+}
