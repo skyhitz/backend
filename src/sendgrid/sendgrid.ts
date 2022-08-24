7022b82c7d30bbd5172386521d293eec475eb1ac
Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
2022-08-24 20:25:23 +0200

    Update mail login link (#46)
    * Sync (#40) (#41)
    
    * Staging (#42)
    
    * Sync (#40)
    
    * add cors
    
    * try public
    
    * Staging (#43)
    
    * Sync (#40)
    
    * remove equal
    
    * return timestamp for visibility
    
    * fix bug
    
    * update
    
    * invert
    
    * final refactor
    
    * updates to match SEP-01
    
    * response with 200
    
    * update readme
    
    * update readme
    
    * Changed login link
    
    Co-authored-by: Alejo Mendoza <alejomendoza@users.noreply.github.com>
    Co-authored-by: Alejo Mendoza <alejandro@skyhitzmusic.com>

--- src/sendgrid/sendgrid.ts
+++ src/sendgrid/sendgrid.ts
@@ -1,62 +1,60 @@
-import { MailDataRequired } from '@sendgrid/helpers/classes/mail';
-import sgMail from '@sendgrid/mail';
-import { Config } from '../config';
-sgMail.setApiKey(Config.SENDGRID_API_KEY);
+import { MailDataRequired } from "@sendgrid/helpers/classes/mail"
+import sgMail from "@sendgrid/mail"
+import { Config } from "../config"
+sgMail.setApiKey(Config.SENDGRID_API_KEY)
 
 class SendGridService {
   constructor() {}
 
   sendEmail(message: MailDataRequired) {
-    return sgMail.send(message);
+    return sgMail.send(message)
   }
 }
 
-export const sendGridService = new SendGridService();
+export const sendGridService = new SendGridService()
 
 export function sendNftSoldEmail(email) {
   return sendGridService.sendEmail({
     to: email,
-    from: { email: 'hello@skyhitz.io', name: 'Skyhitz' },
-    subject: '¡You just sold a music NFT!',
-    templateId: 'd-6687be08e2934811b986c23132b548c1',
-  });
+    from: { email: "hello@skyhitz.io", name: "Skyhitz" },
+    subject: "¡You just sold a music NFT!",
+    templateId: "d-6687be08e2934811b986c23132b548c1",
+  })
 }
 
 export function sendNftBoughtEmail(email) {
   return sendGridService.sendEmail({
     to: email,
-    from: { email: 'hello@skyhitz.io', name: 'Skyhitz' },
-    subject: '¡Now you own a music NFT!',
-    templateId: 'd-0d6857da22a54950b1350666181393da',
-  });
+    from: { email: "hello@skyhitz.io", name: "Skyhitz" },
+    subject: "¡Now you own a music NFT!",
+    templateId: "d-0d6857da22a54950b1350666181393da",
+  })
 }
 
 export function sendWelcomeEmail(email) {
   return sendGridService.sendEmail({
     to: email,
-    from: { email: 'hello@skyhitz.io', name: 'Skyhitz' },
-    subject: 'Welcome to Skyhitz',
-    templateId: 'd-08b9dce0c7d94526aeee9ec06dc1994d',
-  });
+    from: { email: "hello@skyhitz.io", name: "Skyhitz" },
+    subject: "Welcome to Skyhitz",
+    templateId: "d-08b9dce0c7d94526aeee9ec06dc1994d",
+  })
 }
 
 export async function sendLoginEmail(currentUser, token) {
   return await sendGridService.sendEmail({
     to: currentUser.email,
-    from: { email: 'hello@skyhitz.io', name: 'Skyhitz' },
-    subject: 'Log In To Your Skyhitz Account',
-    templateId: 'd-906d105dea7e43d79d8df30c739137a1',
+    from: { email: "hello@skyhitz.io", name: "Skyhitz" },
+    subject: "Log In To Your Skyhitz Account",
+    templateId: "d-906d105dea7e43d79d8df30c739137a1",
     personalizations: [
       {
         to: [{ email: currentUser.email }],
         dynamicTemplateData: {
           login_link: `${
             Config.APP_URL
-          }/accounts/sign-in?token=${token}&uid=${encodeURIComponent(
-            currentUser.id
-          )}`,
+          }/sign-in?token=${token}&uid=${encodeURIComponent(currentUser.id)}`,
         },
       },
     ],
-  });
+  })
 }
