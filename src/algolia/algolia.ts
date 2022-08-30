983c2046900ed5aa1133111732d8f1496fa40717
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-08-30 16:35:40 -0500

    add user id and entry id

--- src/algolia/algolia.ts
+++ src/algolia/algolia.ts
@@ -161,10 +161,12 @@ export async function likeMulti(userId, entryId) {
       await likesIndex.saveObject({
         objectID: `user${userId}entry${entryId}`,
         likeCount: likeCountNumber ? likeCountNumber : 0,
+        userId: userId,
       }),
       await likesIndex.saveObject({
         objectID: `entry${entryId}user${userId}`,
         likeCount: likeCountNumber ? likeCountNumber : 0,
+        entryId: entryId,
       }),
       await entriesIndex.partialUpdateObject({
         objectID: entryId,
