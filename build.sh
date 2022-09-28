e9b2c8ae291992056b8b32f5198b9955c585942a
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-09-28 15:58:58 +0200

    revert changes

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
-#   # Proceed with the build
-#   exit 1;
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
+  # Proceed with the build
+  exit 1;
 
-# else
-#   # Don't build
-#   exit 0;
-# fi
-exit 1;
+else
+  # Don't build
+  exit 0;
+fi
