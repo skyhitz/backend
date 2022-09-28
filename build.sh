9c1427a0854de656101dc2193877d453f4231935
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-09-28 15:48:39 +0200

    trigger build

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
-  # Proceed with the build
-  exit 1;
+# if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
+#   # Proceed with the build
+#   exit 1;
 
-else
-  # Don't build
-  exit 0;
-fi
+# else
+#   # Don't build
+#   exit 0;
+# fi
+exit 1;
