b8bb93526767816d1ba8b105640f3fed7f78d369
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-11-03 10:48:27 +0100

    Added endpoint that generates jwt access token for audible service

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -27,6 +27,7 @@ import GetIssuer from './get-issuer';
 import SetLastPlayedEntry from './set-last-played-entry';
 import SignInWithXDR from './sing-in-with-xdr';
 import ChangeWallet from './change-wallet';
+import GetAudibleToken from './get-audible-token';
 
 const Query = new GraphQLObjectType({
   name: 'Query',
@@ -44,6 +45,7 @@ const Query = new GraphQLObjectType({
       userLikes: UserLikes,
       xlmPrice: XLMPrice,
       getIssuer: GetIssuer,
+      getAudibleToken: GetAudibleToken,
     };
   },
 });
