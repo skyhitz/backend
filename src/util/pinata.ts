900e9cf4795aee96fb0b7b410e5c7aabfd0f1427
Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
2022-11-07 17:01:09 +0100

    FEATURE: Pin videos (#66)
    * Sync (#40) (#41)
    
    * Staging (#42)
    
    * Sync (#40)
    
    * add cors
    
    * try public
    
    * Staging (#43)
    
    * Sync (#40)
    
    * remove equal
    
    * return timestamp for visibility
    
    * fix bug
    
    * update
    
    * invert
    
    * final refactor
    
    * updates to match SEP-01
    
    * response with 200
    
    * update readme
    
    * update readme
    
    * FIX: Sign-up error msg (#45)
    
    * FIX: Sign-up error msg
    
    * FIX: whoops
    
    * add user id and entry id
    
    * update
    
    * HOTFIX: username-duplication (#50)
    
    Someone could create user with already taken username by changing some letter to upper case.
    
    * FEATURE: Pinata (#62) (#63)
    
    * Sync (#40) (#41)
    
    * Staging (#42)
    
    * Sync (#40)
    
    * add cors
    
    * try public
    
    * Staging (#43)
    
    * Sync (#40)
    
    * remove equal
    
    * return timestamp for visibility
    
    * fix bug
    
    * update
    
    * invert
    
    * final refactor
    
    * updates to match SEP-01
    
    * response with 200
    
    * update readme
    
    * update readme
    
    * FIX: Sign-up error msg (#45)
    
    * FIX: Sign-up error msg
    
    * FIX: whoops
    
    * add user id and entry id
    
    * update
    
    * HOTFIX: username-duplication (#50)
    
    Someone could create user with already taken username by changing some letter to upper case.
    
    * Added id to facet attributes
    
    * Added pinning images to pinata
    
    Co-authored-by: Alejo Mendoza <alejomendoza@users.noreply.github.com>
    Co-authored-by: Alejo Mendoza <alejandro@skyhitzmusic.com>
    Co-authored-by: Kacper Ślusarczyk <co123@interia.eu>
    Co-authored-by: Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>
    
    Co-authored-by: Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
    Co-authored-by: Kacper Ślusarczyk <co123@interia.eu>
    Co-authored-by: Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>
    
    * Staging (#65)
    
    * FEATURE: Pinata (#62)
    
    * Sync (#40) (#41)
    
    * Staging (#42)
    
    * Sync (#40)
    
    * add cors
    
    * try public
    
    * Staging (#43)
    
    * Sync (#40)
    
    * remove equal
    
    * return timestamp for visibility
    
    * fix bug
    
    * update
    
    * invert
    
    * final refactor
    
    * updates to match SEP-01
    
    * response with 200
    
    * update readme
    
    * update readme
    
    * FIX: Sign-up error msg (#45)
    
    * FIX: Sign-up error msg
    
    * FIX: whoops
    
    * add user id and entry id
    
    * update
    
    * HOTFIX: username-duplication (#50)
    
    Someone could create user with already taken username by changing some letter to upper case.
    
    * Added id to facet attributes
    
    * Added pinning images to pinata
    
    Co-authored-by: Alejo Mendoza <alejomendoza@users.noreply.github.com>
    Co-authored-by: Alejo Mendoza <alejandro@skyhitzmusic.com>
    Co-authored-by: Kacper Ślusarczyk <co123@interia.eu>
    Co-authored-by: Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>
    
    * Fixed sell offer price and amount (#64)
    
    Co-authored-by: Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
    Co-authored-by: Kacper Ślusarczyk <co123@interia.eu>
    Co-authored-by: Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>
    
    * Added pin ipfs video
    
    * cleanup
    
    * use pin function for update user
    
    Co-authored-by: Alejo Mendoza <alejomendoza@users.noreply.github.com>
    Co-authored-by: Alejo Mendoza <alejandro@skyhitzmusic.com>
    Co-authored-by: Kacper Ślusarczyk <co123@interia.eu>
    Co-authored-by: Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>

---
+++ src/util/pinata.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { Config } from 'src/config';
+import { pinataApi } from 'src/constants/constants';
+
+export async function pinIpfsFile(
+  ipfsHash: string,
+  name: string
+): Promise<unknown> {
+  return await axios
+    .post(
+      `${pinataApi}/pinning/pinByHash`,
+      {
+        hashToPin: ipfsHash,
+        pinataMetadata: {
+          name: name,
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${Config.PINATA_JWT}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    .then(({ data }) => data)
+    .catch((error) => {
+      console.log(error);
+      return null;
+    });
+}
